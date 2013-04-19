bf64a69f08cb928e85de52817e6a2c360e91be79
Benno van den Berg <bennovandenberg@gmail.com>
2013-04-19 10:55:33 +0000

    Add validation on authkey

--- test/ctor-specs.js
+++ test/ctor-specs.js
@@ -1,5 +1,6 @@
 var buster = require("buster");
 var OpenShiftClient = require("../index");
+var a = require("./test-constants");
 
 require("buster-test");
 
@@ -8,7 +9,7 @@ var describe = buster.spec.describe;
 var it = buster.spec.it;
 var before = buster.spec.before;
 
-var spec = describe("Creating client", function(){
+var spec = describe("Creating client", function() {
     "use strict";
     describe("with no parameters", function() {
         before(function() {
@@ -17,35 +18,43 @@ var spec = describe("Creating client", function(){
             };
         });
 
-        it("should throw", function(){
+        it("should throw", function() {
             assert.exception(this.creatingWithNoParameters);
         });
     });
 
-    describe("with a single parameter", function(){
-        before(function() {
-            this.authKey = "ABC";
-            this.client = new OpenShiftClient(this.authKey);
+    describe("with a single parameter", function() {
+        describe("which is a valid authKey", function() {
+            before(function() {
+                console.log('test '+ a.validAuthKey);
+                this.client = new OpenShiftClient(a.validAuthKey);
+            });
+
+            it("should set authKey", function() {
+                assert.equals(this.client.authKey, a.validAuthKey);
+            });
         });
-
-
-        it("should set authKey", function(){
-            assert.equals(this.client.authKey, this.authKey);
+        describe("which is a invalid authKey", function() {
+            it("should throw error", function() {
+                assert.exception(function() {
+                    var client = new OpenShiftClient("invalid auth key");
+                })
+            });
         });
     });
 
-    describe("with two parameters", function(){
+    describe("with two parameters", function() {
         before(function() {
             this.username = "username";
             this.password = "password";
             this.client = new OpenShiftClient(this.username, this.password);
         });
 
-        it("should set username", function(){
+        it("should set username", function() {
             assert.equals(this.client.username, this.username);
         });
 
-        it("should set password", function(){
+        it("should set password", function() {
             assert.equals(this.client.password, this.password);
         });
     });
