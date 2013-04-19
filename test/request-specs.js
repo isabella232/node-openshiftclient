1555dd985fa18fbb5f94145fdeb8d2511f90a5e5
Pieter Joost van de Sande <pj@born2code.net>
2013-04-19 15:11:30 +0200

    Merge authKey const in request-specs

--- test/request-specs.js
+++ test/request-specs.js
@@ -4,22 +4,47 @@ var constants = require("./test-constants");
 
 buster.spec.expose();
 
-var spec = describe("The OpenShiftClient with authKey", function(){
+var spec = describe("The OpenShiftClient", function(){
     "use strict";
-    before(function() {
-        this.client = new OpenShiftClient(constants.validAuthKey);
-    });
 
-    it("includes authKey in authorization header", function(){
-        var options;
+    var options;
+    before(function(){
         this.client._doRequest = function() {
             var callback = arguments[arguments.length-1];
             options = arguments[0];
         };
+    });
 
-        this.client.listDomains();
+    describe("with a authKey", function(){
+        "use strict";
 
-        var expected = "Bearer "+ constants.validAuthKey;
-        assert.equals(options.headers.Authorization, expected);
+        before(function() {
+            this.client = new OpenShiftClient(constants.validAuthKey);
+        });
+
+        it("includes authKey in authorization header", function(){
+            this.client.listDomains();
+
+            var expected = "Bearer "+this.client.authKey;
+            assert.equals(options.headers.Authorization, expected);
+        });
+    });
+
+    describe("with a username or password", function(){
+        "use strict";
+
+        before(function() {
+            this.username = "username";
+            this.password = "password";
+            this.client = new OpenShiftClient(username, password);
+        });
+
+        it("includes authKey in authorization header", function(){
+            this.client.listDomains();
+
+            var base64 = new Buffer(this.username+":"+this.password).toString('base64');
+            var expected = "Basic "+ base64;
+            assert.equals(options.headers.Authorization, expected);
+        });
     });
 });
