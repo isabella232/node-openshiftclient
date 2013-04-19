67b6deb74bd8932b848a60b4855402048b26c284
Benno van den Berg <bennovandenberg@gmail.com>
2013-04-19 13:07:34 +0200

    Use validAuthKey from the constants

--- test/request-specs.js
+++ test/request-specs.js
@@ -1,5 +1,6 @@
 var buster = require("buster");
 var OpenShiftClient = require("../index");
+var constants = require("./test-constants");
 
 require("buster-test");
 
@@ -11,7 +12,7 @@ var before = buster.spec.before;
 var spec = describe("The OpenShiftClient with authKey", function(){
     "use strict";
     before(function() {
-        this.client = new OpenShiftClient("abc");
+        this.client = new OpenShiftClient(constants.validAuthKey);
     });
 
     it("includes authKey in authorization header", function(){
@@ -23,7 +24,7 @@ var spec = describe("The OpenShiftClient with authKey", function(){
 
         this.client.listDomains();
 
-        var expected = "Bearer "+this.client.authKey;
+        var expected = "Bearer "+ constants.validAuthKey;
         assert.equals(options.headers.Authorization, expected);
     });
 });
